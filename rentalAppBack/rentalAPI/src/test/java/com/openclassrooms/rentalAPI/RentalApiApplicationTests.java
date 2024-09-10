f3049fb106e3691164c0088617b1d82cffee7dc2
Adrien Desnoux <adrien.desnoux@gmail.com>
2024-09-10 15:57:53 +0200

    Spring boot initialisation

---
+++ rentalAppBack/rentalAPI/src/test/java/com/openclassrooms/rentalAPI/RentalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.rentalAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
